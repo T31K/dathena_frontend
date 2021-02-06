c17850e1d3795f86c16e4b4d0a2e60c1faee0f3a
T31K <t31kmunwong@gmail.com>
2021-02-06 14:55:42 +0800

    Added background effect using particlesjs (inspiration from Dathena website)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from 'react-particles-js';
+import data from "./particlesconfig.json"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>hello</h1>
+    <Particles params={data}/>
     </div>
   );
 }
