78c324b91d6041b9706fb21d6d8622e1d843dc0c
T31K <t31kmunwong@gmail.com>
2021-02-07 23:19:03 +0800

    Fetch & setState

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 // import Particles from 'react-particles-js';
 
 // import data from "../utils/particlesconfig.json"
@@ -7,17 +7,31 @@ import "../App.css"
 import axios from 'axios';
 
 function Home() {
+  const [user, setUser] = useState([])
   const url = "http://localhost:8000/user/"
+  
   useEffect(() => {
    axios.get(url)
-     .then(res => console.log(res.data))
-  })
+     .then(res => {
+       const users = res.data
+       setUser(users)
+     })
+  }, [])
+  
 
   return (
     <div className="App">
       {/* <Particles params={data} className="abs"/> */}
       <div className="home">
         <h1>hello</h1>
+        
+        {user.map(user => (
+          <div key={user.id}>
+            <p>{user.firstName} {user.lastName} </p>
+          </div>
+        ))
+        }
+        
       </div>
     </div>
   );
