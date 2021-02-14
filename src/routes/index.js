9d6cc6157e259b543ec699718c459c7648174cce
T31K <t31kmunwong@gmail.com>
2021-02-14 17:04:20 +0800

    Updated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "../views/Home";
 import Create from "../views/Create";
+import Edit from "../views/Edit";
 
 export default [
   <Route exact key="home" path="/" component={Home} />,
   <Route exact key="create" path="/create" component={Create} />,
+  <Route exact key="edit" path="/edit/:id" component={Edit} />,
 ]
