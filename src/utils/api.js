484b56694f1fdcd6dff3bb5f9743fd68069bc7a2
T31K <t31kmunwong@gmail.com>
2021-02-15 15:23:21 +0800

    Clean up code

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
 import axios from "axios"
+import Moment from 'moment'
+
 const url = "http://localhost:8000/user/"
 
 const axiosInstance = axios.create({
     baseURL: url
 })
-
 axiosInstance.defaults.headers.common['Content-Type'] = 'application/json'
 
 export default axiosInstance
