5f3dda76a2d608a3f68d2bac5913c0926558df89
T31K <t31kmunwong@gmail.com>
2021-02-15 15:22:54 +0800

    Update intial theme configs

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,7 +1,7 @@
 export const lightTheme  = {
     rainbow: {
         palette: {
-            mainBackground: '#fff',
+            mainBackground: 'rgba(255, 255, 255, 0.40)',
             text: '#8d2d85',
             brand: '#0C72D9'
         },
