3c1303dc7cf8af2965222dcda05dfeb1d0fac545
T31K <t31kmunwong@gmail.com>
2021-02-15 15:24:59 +0800

    Update notification initial configs

---
+++ src/utils/notifications.js
@@ -0,0 +1,32 @@
+export const notifications = {
+    initial : {
+        title: "",
+        message: "",
+        icon: "warning"
+    },
+    updateSuccess : {
+        title: "Success!",
+        message: "User info updated",
+        icon: "success"
+    },
+    createSuccess:{
+        title: "Success!",
+        message: "User added to dabase",
+        icon: "success"        
+    },
+    updateError:{
+        title: "Error",
+        message: "Please try again",
+        icon: "error"
+    },
+    createError:{
+        title: "Error",
+        message: "Please try again",
+        icon: "error"
+    },
+    redirect: {
+        title: "Redirect",
+        message: "Redirecting to homepage",
+        icon: "info"
+    }
+}
