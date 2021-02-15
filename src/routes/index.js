c16599dd485ed59c4c4d5a38c2614108a8f70aa0
T31K <t31kmunwong@gmail.com>
2021-02-15 15:27:23 +0800

    Added new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Home from "../views/Home";
-import Create from "../views/Create";
-import Edit from "../views/Edit";
+import React from "react"
+import { Route } from "react-router-dom"
+import Home from "../views/Home"
+import Create from "../views/Create"
+import Edit from "../views/Edit"
+import NotFound from "../views/NotFound"
+import Help from "../views/Help"
 
-export default [
+const routes = [
   <Route exact key="home" path="/" component={Home} />,
   <Route exact key="create" path="/create" component={Create} />,
   <Route exact key="edit" path="/edit/:id" component={Edit} />,
+  <Route exact key="help" path="/help" component={Help} />,
+  <Route exact key="notfound" path="*" component={NotFound} />,
 ]
+
+export default routes
